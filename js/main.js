2dc7b97abc6cf749881c5c38d7797e448d95059b
Nalgotithm <nikita.logvinenko@mail.ru>
2020-11-28 02:14:58 +0300

    Add js events

--- js/main.js
+++ js/main.js
@@ -1,82 +1,26 @@
-function swap(arr, i, j) {
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-}
+function initModal(wrapperId, showBtnId) {
+    let modalWrapper = document.getElementById(wrapperId)
+    let closeBtn = modalWrapper.getElementsByClassName("modal-close")[0]
+    let bg = modalWrapper.getElementsByClassName("modal-bg")[0]
+    let showBtn = document.getElementById(showBtnId)
+    let body = document.getElementsByTagName("body")[0]
 
-function findMin(arr)
-{
-    var min = arr[0]
-    for(let el in arr)
-    {
-        if(arr[el] < min)
-        {
-            min = arr[el]
-        }
-    }
-    return min
-}
+    showBtn.addEventListener("click", function(e) {
+        body.classList.add("disable-scroll")
+        modalWrapper.classList.add("modal-wrapper-show")
+    })
 
-function findMax(arr)
-{
-    var max = arr[0]
-    for(let el in arr)
-    {
-        if(arr[el] > max)
-        {
-            max = arr[el]
-        }
-    }
-    return max
-}
+    closeBtn.addEventListener("click", function(e) {
+        body.classList.remove("disable-scroll")
+        modalWrapper.classList.remove("modal-wrapper-show")
+    })
 
-function partition(arr, l, r)
-{
-    var pivot = arr[Math.floor((l + r) / 2)]
-    var [i, j] = [l, r]
-    while (i <= j)
-    {
-        while (arr[i] < pivot)
-            i++
-        while (arr[j] > pivot)
-            j--
-        if (i >= j)
-            break
-        [arr[i], arr[j]] = [arr[j], arr[i]]
-        i++, j--
-    }
-    return j
-}
-
-function quicksort(a, l, r)
-{
-    if (l < r)
-    {
-        let p = partition(a, l, r)
-        quicksort(a, l, p)
-        quicksort(a, p + 1, r)
-    }
-}
-
-function countTegs()
-{
-    var els = document.getElementsByTagName("*");
-    var tags = []
-
-    for (let i = 0; i < els.length; i++) {
-        tags.push(els[i].tagName)
-    }
-    let counts = {}
-    tags.forEach(function(x) { counts[x] = (counts[x] || 0)+1; })
-    console.log(counts)
+    bg.addEventListener("click", function(e) {
+        body.classList.remove("disable-scroll")
+        modalWrapper.classList.remove("modal-wrapper-show")
+    })
 }
 
 
-var arr = Array.from({length: 1000}, () => Math.floor(Math.random() * 1000))
-console.log(arr)
-
-console.log(findMin(arr))
-console.log(findMax(arr))
-
-quicksort(arr, 0, arr.length - 1)
-console.log(arr)
-
-countTegs()
+initModal("informationWrapper", "showModal")
+initModal("avatarWrapper", "photo")
